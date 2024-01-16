14ab752bdeca691a5c8ddf994dae0e616fb0a5a0
harish <harishbarhate29@gmail.com>
2024-01-16 17:11:44 +0530

    16-01-h

---
+++ lib/widgets/dashbox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class dashbox extends StatelessWidget {
+  const dashbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
